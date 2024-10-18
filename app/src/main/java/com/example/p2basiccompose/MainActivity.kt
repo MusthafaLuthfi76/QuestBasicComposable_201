9f552c1933d08740d8061b01c509c5d479b01d24
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-18 22:20:02 +0700

    Menambahkan teks nama dan mengatur spacer (jarak) antara nama dan logo

--- app/src/main/java/com/example/p2basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2basiccompose/MainActivity.kt
@@ -72,6 +72,12 @@ fun BasicColumn(modifier: Modifier = Modifier){
             contentDescription = "",
             modifier = Modifier.clip(CircleShape).size(250.dp)
         )
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Text(
+            text = "Nama",
+            fontSize = 24.sp,
+        )
     }
 }
 
