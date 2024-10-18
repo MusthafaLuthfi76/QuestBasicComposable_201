70b1f1ffd0cccb5bf3f381c039c3a52decee1720
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-18 22:17:11 +0700

    Mengatur ukuran Logo

--- app/src/main/java/com/example/p2basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2basiccompose/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -69,7 +70,7 @@ fun BasicColumn(modifier: Modifier = Modifier){
         Image(
             painter = painterResource(id = R.drawable.logou),
             contentDescription = "",
-            modifier = Modifier.clip(CircleShape)
+            modifier = Modifier.clip(CircleShape).size(250.dp)
         )
     }
 }
